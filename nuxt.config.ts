20d92e3238e0e234337643299871af4a8a220a0c
iptton <iptton@gmail.com>
2024-09-14 06:32:25 +0000

    set baseURL for githubpages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import * as daisyui from "daisyui";
+
+const baseURL = `${process.env.BASEURL ?? "/"}`
+console.info('baseURL='+baseURL, process.env.BASEURL)
 export default defineNuxtConfig({
+  app: {
+    baseURL: baseURL
+  },
   components: [
     {
       path: "~/components",
       pathPrefix: true
     },
   ],
-  compatibilityDate: '2024-07-03',
+  compatibilityDate: "2024-09-14",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
